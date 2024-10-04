8db793934ad70a3b42fd2730ed89d3fdc2857b3a
Arnav Gupta <arnavgupta9000@gmail.com>
2024-10-03 21:51:03 -0600

    added the nav bar and added a login page with css (still need to add the forum and the post requests)

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html') # can also pass show_nav_bar = True, as an arg and it will work
 
 @routes.route('/books')
 def books():
